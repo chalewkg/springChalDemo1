41d39b62b3bb1fa312aa6f54670c538ee931914c
chalewkg <chalewkg@gmail.com>
2022-06-15 22:27:26 -0400

    initail Commit

---
+++ src/main/java/com/chalew/spring5WebApp/Spring5WebAppApplication.java
@@ -0,0 +1,14 @@
+package com.chalew.spring5WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Spring5WebAppApplication.class, args);
+	}
+
+}
